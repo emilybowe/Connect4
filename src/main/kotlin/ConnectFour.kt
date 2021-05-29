bdef422dd338fef976c3b3573eb0b7ffd2562fba
EmilyBowe <ebowe@tcd.ie>
2021-05-29 21:42:24 +0100

    Added checkWinner, breaking code out into more SoC

--- src/main/kotlin/ConnectFour.kt
+++ src/main/kotlin/ConnectFour.kt
@@ -2,15 +2,27 @@ import kotlin.random.Random
 
 class ConnectFour {
     var board = arrayOf<Array<Int>>()
+    var opponantX : Int = Random.nextInt(0, 5)
+    var opponantY : Int = Random.nextInt(0, 6)
+    var playerX = Integer.valueOf(readLine())
+    var playerY = Integer.valueOf(readLine())
 
 
     fun play() {
-        program()
-        driver()
+        setup()
+        do {
+            game()
+            if()
+        } while (1==1)
+
+    }
+
+    fun game() {
+        player()
         opponant()
     }
 
-    fun program() {
+    fun setup() {
 
         for (i in 0..5) {
             var array = arrayOf<Int>()
@@ -32,21 +44,27 @@ class ConnectFour {
         }
     }
 
-    fun driver() {
+    fun player() {
         println("Your move")
-        var Xinput = Integer.valueOf(readLine())
-        var Yinput = Integer.valueOf(readLine())
-        board[Xinput][Yinput] = 1
+        board[playerX][playerY] = 1
         printBoard()
+        checkWinner(playerX, playerY)
+    }
+
+    fun checkWinner(xValue: Int, yValue: Int) {
+
     }
 
     fun opponant() {
         println()
         println("Now my turn....hmm")
+        println()
         Thread.sleep(5_000)
         println("ok I'm moving")
-        board[Random.nextInt(0, 5)][Random.nextInt(0, 6)] = 1
+        Thread.sleep(1_000)
+        board[opponantX][opponantY] = 2
         printBoard()
+        checkWinner(opponantX, opponantY)
     }
 }
 
