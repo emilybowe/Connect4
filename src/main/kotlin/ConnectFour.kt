41ad3a358f400137e0e7fee482521643a76a4ade
EmilyBowe <ebowe@tcd.ie>
2021-05-31 21:26:52 +0100

    Adding checkForWinner, have included while loop and structure

--- src/main/kotlin/ConnectFour.kt
+++ src/main/kotlin/ConnectFour.kt
@@ -1,3 +1,6 @@
+import java.util.*
+import kotlin.random.Random
+
 fun main(args: Array<String>) {
     var testList : MutableList<Int> = mutableListOf<Int>()
     testList.add(0, 0)
@@ -20,41 +23,21 @@ class ConnectFour {
     var opponentsCounter = 2
     var xValue : Int = 0
     var yValue : Int = 0
+    var xMax : Int = 5
+    var yMax : Int = 6
 
-    fun playConnectFour(moveList : MutableList<Int> = mutableListOf<Int>()) {
-        setup(moveList)
-        play(moveList)
+    fun playConnectFour(playerMoveList : MutableList<Int> = mutableListOf<Int>()) {
+        setup(playerMoveList)
+        play(playerMoveList)
     }
 
-    fun setup(moveList : MutableList<Int> = mutableListOf<Int>()) {
+    fun setup(playerMoveList : MutableList<Int> = mutableListOf<Int>()) {
         setupBoard()
-        setupPlayerMoves(moveList)
-        setupOpponentMoves()
-    }
-
-    fun play(moveList : MutableList<Int> = mutableListOf<Int>()) {
-        usePlayerMove()
     }
 
     fun setupBoard(){
         buildBoard()
         printBoard()
-    }
-
-    fun setupPlayerMoves(moveList : MutableList<Int> = mutableListOf<Int>()) {
-        getNextMoveForPlayer(moveList)
-        addPlayerMoveToList(xValue, yValue, moveList)
-    }
-
-    fun usePlayerMove() {
-        useMoveOnPlayerList()
-        removePlayerMoveThatHasBeenUsed()
-    }
-
-    fun setupOpponentMoves() {
-        generateOpponentMove()
-        //var opponentX : Int = Random.nextInt(0, 5)
-        //var opponentY : Int = Random.nextInt(0, 6)
     }
 
     fun buildBoard() {
@@ -76,33 +59,112 @@ class ConnectFour {
         }
     }
 
-    fun getNextMoveForPlayer(moveList : MutableList<Int> = mutableListOf<Int>()) {
-        if(moveList.isEmpty()) {
+    fun play(playerMoveList : MutableList<Int> = mutableListOf<Int>()) {
+        while (true) {
+            setupPlayerMoves(playerMoveList)
+            setupOpponentMoves()
+            useAndRemovePlayerMove()
+            useAndRemoveOpponentMove()
+
+            if() {
+                break
+            }
+        }
+
+    }
+
+    fun checkForWinner() {
+
+    }
+
+    fun setupPlayerMoves(playerMoveList : MutableList<Int> = mutableListOf<Int>()) {
+        getNextMoveForPlayer(playerMoveList)
+        addPlayerMoveToList(xValue, yValue, playerMoveList)
+    }
+
+    fun setupOpponentMoves(opponentMoveList : MutableList<Int> = mutableListOf<Int>()) {
+        getNextMoveForOpponent(opponentMoveList)
+        addOpponentMoveToList(xValue, yValue, opponentMoveList)
+    }
+
+    fun getNextMoveForPlayer(playerMoveList : MutableList<Int> = mutableListOf<Int>()) {
+        if(playerMoveList.isEmpty()) {
             xValue = Integer.valueOf(readLine())
             yValue = Integer.valueOf(readLine())
             addPlayerMoveToList(xValue, yValue)
         }
     }
 
-    fun addPlayerMoveToList(xValue: Int, yValue: Int, moveList: MutableList<Int> = mutableListOf<Int>()) {
-        moveList.add(xValue)
-        moveList.add(yValue)
-    }
-
-    fun useMoveOnPlayerList(moveList : MutableList<Int> = mutableListOf<Int>()) {
-        board[moveList[0]][moveList[1]] = playersCounter
-    }
-
-    fun removePlayerMoveThatHasBeenUsed(moveList : MutableList<Int> = mutableListOf<Int>()) {
-        if(moveList.isNotEmpty()) {
-            moveList.remove(0)
-            moveList.remove(1)
+    fun getNextMoveForOpponent(opponentMoveList : MutableList<Int> = mutableListOf<Int>()) {
+        if(opponentMoveList.isEmpty()) {
+            xValue = generateRandomXValue()
+            yValue = generateRandomYValue()
+            addOpponentMoveToList(xValue, yValue)
         }
     }
 
-    fun generateOpponentMove(opponentX: Int, opponentY: Int) {
-        board[opponentX][opponentY] = opponentsCounter
+    fun addPlayerMoveToList(xValue: Int, yValue: Int, playerMoveList: MutableList<Int> = mutableListOf<Int>()) {
+        playerMoveList.add(xValue)
+        playerMoveList.add(yValue)
     }
 
+    fun addOpponentMoveToList(xValue: Int, yValue: Int, opponentMoveList: MutableList<Int> = mutableListOf<Int>()) {
+        opponentMoveList.add(xValue)
+        opponentMoveList.add(yValue)
+    }
+
+    fun useAndRemovePlayerMove(playerMoveList : MutableList<Int> = mutableListOf<Int>()) {
+        useMoveOnPlayerList(playerMoveList)
+        removePlayerMoveThatHasBeenUsed(playerMoveList)
+    }
+
+    fun useAndRemoveOpponentMove(opponentMoveList : MutableList<Int> = mutableListOf<Int>()) {
+        useMoveOnOpponentList(opponentMoveList)
+        removeOpponentMoveThatHasBeenUsed(opponentMoveList)
+    }
+
+    fun useMoveOnPlayerList(playerMoveList : MutableList<Int> = mutableListOf<Int>()) {
+        if(isThisBoardPositionAlreadyTaken(playerMoveList[0], playerMoveList[1])) {
+            board[generateRandomXValue()][generateRandomYValue()] = playersCounter
+        } else {
+            board[playerMoveList[0]][playerMoveList[1]] = playersCounter
+        }
+
+    }
+
+    fun useMoveOnOpponentList(opponentMoveList : MutableList<Int> = mutableListOf<Int>()) {
+        if(isThisBoardPositionAlreadyTaken(opponentMoveList[0], opponentMoveList[1])) {
+            board[generateRandomXValue()][generateRandomYValue()] = opponentsCounter
+        } else {
+            board[opponentMoveList[0]][opponentMoveList[1]] = opponentsCounter
+        }
+
+    }
+
+    fun isThisBoardPositionAlreadyTaken(x: Int, y: Int) : Boolean{
+        return board[x][y] != 0
+    }
+
+    fun generateRandomXValue(): Int {
+        return Random.nextInt(0, xMax)
+    }
+
+    fun generateRandomYValue(): Int {
+        return Random.nextInt(0, yMax)
+    }
+
+    fun removePlayerMoveThatHasBeenUsed(playerMoveList : MutableList<Int> = mutableListOf<Int>()) {
+        if(playerMoveList.isNotEmpty()) {
+            playerMoveList.remove(0)
+            playerMoveList.remove(1)
+        }
+    }
+
+    fun removeOpponentMoveThatHasBeenUsed(opponentMoveList : MutableList<Int> = mutableListOf<Int>()) {
+        if(opponentMoveList.isNotEmpty()) {
+            opponentMoveList.remove(0)
+            opponentMoveList.remove(1)
+        }
+    }
 }
 
